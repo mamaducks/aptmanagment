a9b4830c4869b3e62cf9d17fef7896de11a1b777
mamaducks <leonad308@gmail.com>
2022-04-02 15:44:10 -0400

    files deleted

--- src/data/depositAtoms.js
+++
@@ -1,9 +0,0 @@
-import { atom } from "recoil";
-import { data } from "./data";
-import { localStorageEffect } from "./localStorage";
-
-export const allDeposits = atom({
-  key: "allDeposits",
-  default: data.deposits,
-  effects_UNSTABLE: [localStorageEffect("allDeposits", [])],
-});
